1f1f42628b84b18fd3577848ac41638b70060190
Sparsh Amarnani <56154732+NaniDaChaman@users.noreply.github.com>
2024-12-11 19:48:12 +0530

    changing repos for ml_servers

--- Cloud/Setup/push_images.sh
+++ Cloud/Setup/push_images.sh
@@ -2,10 +2,10 @@ cd ..
 cdir=$(pwd)
 echo "Current directory is $cdir"
 declare -A images=([192.168.1.64:5000/couchdb]='CouchDB_Docker/Dockerfile.db' 
-[192.168.1.81:5000/mlmodel]='ML_model/Dockerfile.ml_model'  
+[192.168.1.64:5000/mlmodel]='ML_model/Dockerfile.ml_model'  
 [192.168.1.64:5000/apachekafka]='Kafka_Docker/Kafka/Dockerfile' 
 [192.168.1.64:5000/iot]='IOT/Dockerfile' 
-[192.168.1.81:5000/mlconsumer]='ML_model/Dockerfile.consumer' 
+[192.168.1.64:5000/mlconsumer]='ML_model/Dockerfile.consumer' 
 [192.168.1.64:5000/couchdbconsumer]='CouchDB_Docker/Dockerfile.consumer')
 for image in "${!images[@]}";
 do 
