37e502c667d704c697956d2d56e1b89b4ff0d95e
Sparsh Amarnani <56154732+NaniDaChaman@users.noreply.github.com>
2024-12-11 20:57:00 +0530

    setting up kafka

--- Cloud/Setup/create_pods.sh
+++ Cloud/Setup/create_pods.sh
@@ -1,3 +1,9 @@
+kubectl apply -f apachezk-deployment.yaml -n team13
+kubectl apply -f apachekafka-deployment.yaml -n team13
+kubectl apply -f ml_server.yaml -n team13
+kubectl apply -f couchdb-deployment.yaml -n team13
+sleep 5
+kubectl get pods -n team13
 kubectl apply -f mlconsumer-job.yaml -n team13
 kubectl apply -f arima-test.yaml -n team13
 kubectl apply -f producer-job.yaml -n team13
