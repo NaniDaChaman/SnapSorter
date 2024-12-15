cd03c090ffc5c4018f05081ad86b2ec229ef2129
Sparsh Amarnani <56154732+NaniDaChaman@users.noreply.github.com>
2024-12-15 06:28:39 +0530

    yes

--- Cloud/Spark/MapReduce.py
+++ Cloud/Spark/MapReduce.py
@@ -4,6 +4,82 @@ import requests
 import json
 import numpy
 from time import perf_counter
+# import couchdb
+
+
+# try:#172.16.2.208
+#     # Connect to CouchDB using the provided credentials and URL
+#     couch = couchdb.Server('http://admin:admin@172.16.2.187:30005/')
+    
+#     # Check if the 'img_db' database exists, if not, create it
+#     if 'img_db' not in couch:
+#         db = couch.create('img_db')
+#     else:
+#         db = couch['img_db']
+#     print("Connected to CouchDB successfully.")
+# except Exception as e:
+#     # Handle CouchDB connection error
+#     print(f"Failed to connect to CouchDB: {e}")
+
+# all_docs = db.view('_all_docs', include_docs=True)
+
+# def map1(chunk):
+#     ichunk={}
+#     if chunk['GroundTruth']!=chunk['InferredValue']:
+#         ichunk['producer_id']=1
+#     else : 
+#         ichunk['producer_id']=0
+#     # for doc_id in chunk:
+#     #     gt=chunk[doc_id]['GroundTruth']
+#     #     iv=chunk[doc_id]['InferredValue']
+#     #     pid=chunk[doc_id]['producer_id']
+#     #     ichunks[pid].append({'id':doc_id,'GroundTruth':gt,'InferredValue':iv})
+#     return ichunk
+
+# def reduce1(a,b):
+#     return set(a,b)
+    
+
+# def map2(chunk,n):
+#     ichunk=[0 for i in range(n)]
+
+    
+
+# def reduce(chunk):
+#     ichunks={}
+#     for l in chunk:
+#         ichunks[l]=0
+#         for i in chunk[l]:
+#             if i['GroundTruth']!=i['InferredValue']:
+#                 ichunks[l]=ichunks[l]+1
+#             # else : 
+#             #     ichunks[l].append({i['GroundTruth']:0})
+#     return ichunks.values()
+#         # GroundTruth=chunk[doc_id]['GroundTruth']
+#         # InferredValue=chunk[doc_id]['InferredValue']
+#         # if GroundTruth==InferredValue:
+#         #     if GroundTruth in ichunks:
+#         #         ichunks[GroundTruth]=ichunks[GroundTruth]+1
+#         #     else :
+#         #         ichunks[GroundTruth]=1
+
+# # def reduce(ichunks):
+# #     result=0
+# #     fin_result=[]
+# #     for pid in ichunks:
+# #         fin_result[pid]=0
+# #         for i in ichunks[pid]:
+# #             fin_result[pid]=fin_result[pid]+i.
+# #     return result
+
+# spark = SparkSession \
+#                 .builder \
+#                 .appName("MapReduce") \
+#                 .config('spark.default.parallelism', t[0]) \
+#                 .config('spark.sql.shuffle.partitions', t[1]) \
+#                 .getOrCreate()
+
+# docs_rdd=spark.sparkContext.parallelize(all_docs)
 
 if __name__ == "__main__":
     rowsToRetrieve = 1000000
@@ -21,7 +97,7 @@ if __name__ == "__main__":
     while skip < rowsToRetrieve:
         start = perf_counter()
         raw = requests.get(
-            f'http://admin:123456@129.114.25.15:30005/cloudpa4/_all_docs?include_docs=true&skip={skip}&limit={limit}')
+            f'http://admin:admin@172.16.2.187:30005/img_db/_all_docs?include_docs=true&skip={skip}&limit={limit}')
         end = perf_counter()
         count += 1
         print('Chunk', count, 'Time', end - start, 's')
